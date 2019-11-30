cdd06620ba1035b5c664dc7f65f160f7199f92b6
diego-ferrand <diego.ferrand@abstracta.com.uy>
2019-11-30 20:02:50 -0300

    Advancements to mvp sdk

--- oculow.js
+++ oculow.js
@@ -1,47 +1,85 @@
-var tmp = require('tmp');
-var fs = require('fs'),
+module.exports ={
+  Oculow : class Oculow {
+    constructor(){
+      this.tmp = require('tmp');
+      this.path = require('path');
+      this.fs = require('fs');
+      this.axios = require('axios');
+      
+      this._dir = tmp.dir({ template: 'tmp-XXXXXX' }, function _tempDirCreated(err, path) {
+        if (err) throw err;
+      
+        return path;
+      });
+      this.module_comparison_logic = 1
+      this.module_baseline_management = 1
+      this._execution_id = None
+      this.module_api_key = None
+      this.module_api_secret_key = None
+      this.module_app_id = None
+      this.viewport_width = None
+      this.viewport_height = None
+      this.base_url = "https://us-central1-lince-232621.cloudfunctions.net/"
+      this._report_base_url = "https://dev.oculow.com/dashboard/executions.html"
+      this.execution_status_function = "get_execution_status-dev"  // TODO extract to config file
+      this.process_function = "process_image-dev"  // TODO extract to config file
 
-const axios = require('axios')
+
+    };
+    
+    setComparisonLogic(COMPARISON_LOGIC){
+      this.module_comparison_logic=COMPARISON_LOGIC;
+    }
+    
+    setBaselineManagement(MANAGEMENT_LEVEL){
+      this.module_baseline_management=MANAGEMENT_LEVEL;
+    }
+    setAppId(APP_ID){
+      this.module_app_id=APP_ID;
+    }
+    
+    setApiKey(API_KEY, SECRET_KEY){
+      this.module_api_key=API_KEY;
+      this.module_api_secret_key=SECRET_KEY;
+    }
+    
+    
+    captureScreen (browser, title) {
+      var final_image_path= this.path.join(this._dir,title)
+      browser.saveScreenshot(final_image_path);
+      return uploadImage(final_image_path)
+    }
+    
+    uploadImage(path){
+      let data = new FormData();
+      data.append("file", this.fs.createReadStream(path));
+      data.append('api_key', module_api_key + "__" + module_api_secret_key);
+      data.append('app_id', module_app_id);
+      data.append('comparison_logic', module_comparison_logic);
+      data.append('execution_id', _execution_id);
+      data.append('baseline_management', module_baseline_management);
+      data.append('viewport', json.dumps({"width": viewport_width, "height": viewport_height}));
+    
+      const request_config = {
+        headers: {
+          "Authorization": "Bearer " + access_token,
+          "Content-Type": "multipart/form-data"
+        },
+        data: data
+      };
+      return this.axios.post(base_url+process_function,data, request_config)
+      
+    .then((res) => {
+      console.log(`statusCode: ${res.statusCode}`)
+      console.log(res)
+      return res;
+    })
+    .catch((error) => {
+      console.error(error)
+      return error;
+    })
+    }
 
 
-var _dir = tmp.dir({ template: 'tmp-XXXXXX' }, function _tempDirCreated(err, path) {
-    if (err) throw err;
-   
-    return path;
-  });
-var module_comparison_logic = 1
-var module_baseline_management = 1
-var _execution_id = None
-var module_api_key = None
-var module_api_secret_key = None
-var module_app_id = None
-var viewport_width = None
-var viewport_height = None
-var base_url = "https://us-central1-lince-232621.cloudfunctions.net/"
-var _report_base_url = "https://dev.oculow.com/dashboard/executions.html"
-var execution_status_function = "get_execution_status-dev"  // TODO extract to config file
-var process_function = "process_image-dev"  // TODO extract to config file
-
-
-function captureScreen (browser, title) {
-    browser.saveScreenshot(title);
 }
-
-function uploadImage(path){
-  let data = new FormData();
-  axios.post(base_url+process_function, {
-    'api_key': module_api_key + "__" + module_api_secret_key,
-    'app_id': module_app_id,
-    'comparison_logic': module_comparison_logic,
-    'execution_id': _execution_id,
-    'baseline_management': module_baseline_management,
-    "viewport": json.dumps({"width": viewport_width, "height": viewport_height})
-})
-.then((res) => {
-  console.log(`statusCode: ${res.statusCode}`)
-  console.log(res)
-})
-.catch((error) => {
-  console.error(error)
-})
 }
--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,9 +1,27 @@
 const assert = require('assert')
+var oculow = require('../../oculow.js')
+oculow = new oculow.Oculow();
 
 describe('webdriver.io page', () => {
     it('should have the right title', () => {
         browser.url('https://webdriver.io')
         const title = browser.getTitle()
         assert.strictEqual(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js')
+    })
+})
+
+describe('oculow sdk test', () => {
+    it('should capture and process the image', () => {
+        oculow = oculow.Oculow()
+        oculow.setApiKey()
+        oculow.setAppId()
+        oculow.setBaselineManagement()
+        oculow.setComparisonLogic()
+        
+        browser.url('https://www.oculow.com')
+        var result = oculow.captureScreen(browser, "testing js.png")
+        console.log("RESULT: "+result)
+
+
     })
 })
