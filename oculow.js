aed71af2e4e112a4be27a2f000ba0028b517cfdb
diego-ferrand <diego.ferrand@abstracta.com.uy>
2020-05-14 12:22:17 -0300

    Add donwload image

--- oculow.js
+++ oculow.js
@@ -1,9 +1,11 @@
 const assert = require('assert')
 const POST_METHOD = 'POST';
+const GET_METHOD = 'GET';
 const MULTIPART_FORMDATA = 'multipart/form-data';
 let request = require('request');
+var rp = require('request-promise');
 let fs = require('fs');
-const compareImages = require("resemblejs/compareImages");
+let compareImages = require("resemblejs/compareImages");
 const util = require("util");
 const readFile = util.promisify(fs.readFile);
 function getEnv(name) {
@@ -12,6 +14,7 @@ function getEnv(name) {
 module.exports = {
     Oculow: class Oculow {
         constructor() {
+            console.log("Environment variables:" + process.env.KEY)
             //Detect os environment variables fo oculow and asignconst example: this.apiKey = process.env.KEY || 3000; this.apiSecretKey = process.env.SECRET
             this.apiKey = getEnv("KEY")
             this.apiSecretKey = getEnv("SECRET")
@@ -29,7 +32,7 @@ module.exports = {
             this.IGNORE_AA = 1;
             this.DETECT_ERRORS = 3;
             this._dir = this.tmp.dirSync().name;
-            console.log('Dir: ', this._dir);
+            console.debug('Dir: ', this._dir);
             this.comparisonLogic = 1
             this.baselineManagement = 1
             this.executionId = null
@@ -43,38 +46,48 @@ module.exports = {
             this.executionStatusFunction = "get_execution_status-dev"
             this.processFunction = "process_image-dev"
             this.accFunction = "get_account-dev"
+
+            this.execution = {}
         };
 
         setComparisonLogic(COMPARISON_LOGIC) {
+            console.info("Setting baseline comparison logic")
             this.comparisonLogic = COMPARISON_LOGIC;
         }
 
         setBaselineManagement(MANAGEMENT_LEVEL) {
+            console.info("Setting baseline management level")
             this.baselineManagement = MANAGEMENT_LEVEL;
         }
 
         setExecutionId(EXECUTION_ID){
+            console.info("Setting execution id")
             this.executionId = EXECUTION_ID;
         }
 
         setExecutionStatus(STATUS){
+            console.info("Setting execution status")
             this.executionStatus = STATUS;
         }
         
         setAppId(APP_ID) {
+            console.info("Setting app id")
             this.appId = APP_ID;
         }
 
         setAccId(ACC_ID){
+            console.info("Setting acc id")
             this.accId = ACC_ID;
         }
 
         setKeys(API_KEY, SECRET_KEY) {
+            console.info("Setting keys")
             this.apiKey = API_KEY;
             this.apiSecretKey = SECRET_KEY;
         }
 
         setViewportSize(){
+            console.info("Setting viewport size")
             this.viewportWidth = this.getBrowserWindowSize('width');
             this.viewportHeight = this.getBrowserWindowSize('height');
         }
@@ -116,7 +129,7 @@ module.exports = {
                             return reject(err)
                         }
                         resolve(res)
-                        console.log("Capture screen: ", res.statusCode + " " + res.statusMessage);
+                        console.info("Capture screen: ", res.statusCode + " " + res.statusMessage);
                         let load = JSON.parse(res.body);
                         this.setExecutionId(load.execution_id);
                         this.setAccId(load.acc_id);
@@ -131,19 +144,33 @@ module.exports = {
             if (this.path.extname(title) == '') {
                 title = title + '.png'
             }
-            let final_image_path = this.path.join(this._dir.toString(), title);
-            console.log("Final image path: " + final_image_path);
-            browser.saveScreenshot(final_image_path);
+            this.final_image_path = this.path.join(this._dir.toString(), title);
+            console.info("Captured image in path: " + this.final_image_path);
+            browser.saveScreenshot(this.final_image_path);
             this.setViewportSize();
             // this.uploadImage(final_image_path);
             
             //New code for resemblejs
-            const data = compareImages(final_image_path, final_image_path);
-            console.log("FINALLY COMPARED THESE FUCKERS")
-            console.log(data)
-            assert.equal(data.misMatchPercentage,0)
-            
-
+            this.getAccount()
+            let res_key = this.viewportWidth + '_' + this.viewportHeight 
+            let dict_safe_title = title.replace(".","_").toLowerCase()
+            console.info("Looking for baseline in account data: " + dict_safe_title +"   "+res_key)
+            this.baseline_url = this.getBaselineUrl(dict_safe_title, res_key)
+            if (this.baseline_url == null){
+                console.info("No baseline detected, creating new execution log.")
+                let validation = this.execution.new_validation || []
+                validation.push({"res_key":res_key, "dict_safe_title":dict_safe_title, "save_path":this.final_image_path})
+                this.execution["new_validation"] = validation
+                
+            }else{
+                this.baseline_path = this.final_image_path.replace(".png", "_baseline.png")
+                console.info("Comparing images")
+                this.compareImageToBaseline(browser, this.baseline_url, this.baseline_path)
+                    
+                // console.debug("Image comparison result: ")
+                // console.debug(this.comparison)
+                // assert.equal(this.comparison.misMatchPercentage,0)
+            }
         }
 
 
@@ -181,13 +208,21 @@ module.exports = {
                     console.log("Tests failed, please review at: ", reportURL);
                 }
             }
-            console.log("To view a detailed report of the execution please navigate to: ", this.reportBaseUrl + "?id=" + this.executionId);
+            console.warn("To view a detailed report of the execution please navigate to: ", this.reportBaseUrl + "?id=" + this.executionId);
         }
 
         getAccount(){
-            if (!this.apiKey && !this.apiSecretKey && !this.appId)
+            console.log("Retrieving account details")
+            if (!this.apiKey && !this.apiSecretKey && !this.appId){
+                console.warn("Apikey or appid not set for retrieving account details")
+                console.debug(this.apiKey)
+                console.debug(this.apiSecretKey)
+                console.debug(this.appId)
                 return null;
-            let accURL = this.accFunction + "?api_key=" + this.apiKey + "&secret=" + this.apiSecretKey;
+            }
+            let headers = { };
+            let accURL =this.baseUrl + this.accFunction + "?api_key=" + encodeURIComponent(this.apiKey) + "&secret=" + encodeURIComponent(this.apiSecretKey);
+            console.debug("request url: "+accURL)
             let options = {url: accURL, method: GET_METHOD, headers: headers};
             browser.call(() => {
                 return new Promise((resolve, reject) => {
@@ -196,9 +231,40 @@ module.exports = {
                             return reject(err)
                         }
                         resolve(res)
-                        console.log("Retrieved account", res.statusCode + " " + res.statusMessage);
+                        console.info("Retrieved account", res.statusCode + " " + res.statusMessage);
+                        console.debug(res.body)
                         this.account = JSON.parse(res.body).results;
                         this.accId = this.account.acc_id;
+                    })
+                })
+            })
+        }
+        getBaselineUrl(title, res_key){
+            let baselines = this.account.data.baselines;
+            console.debug("All baselines in app executions");
+            let cond = (title in baselines) && (res_key in baselines[title]);
+            console.debug("Condition: "+cond);
+            if(cond){
+                return baselines[title][res_key]["url"];
+            }
+            return null;
+        }
+
+        compareImageToBaseline(browser, url, path){
+            console.log("Downloading image to " + path)
+            let file = fs.createWriteStream(path);
+            let options = {url: url, method: GET_METHOD, headers: {}};
+            browser.call(() => {
+                return new Promise((resolve, reject) => {
+                    request(options,(err,res) => {
+                        if (err) {
+                            return reject(err)
+                        };
+                        resolve(res)
+                    }).pipe(file).on('finish', () => {
+                        console.log("RESPONSE HERE")
+                    this.comparison = compareImages(this.baseline_path, this.final_image_path)
+                    console.log("comparison actual value", this.comparison)
                     })
                 })
             })
